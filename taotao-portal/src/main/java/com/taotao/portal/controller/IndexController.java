ed0ef4773009276e6b2b5684e07245d75935dfdf
ppc <ppc@ppc-PC>
2018-05-10 22:45:42 +0800

    REST

---
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.taotao.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+
+@Controller
+public class IndexController {
+
+	
+	
+	@RequestMapping("/index")
+	public String Index()
+	{
+		return "index";
+		
+	}
+}
